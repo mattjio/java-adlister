2dc42e0f18a19405b95a682461f042dfde5db5fa
matthew jio <matthew.p.jio1@gmail.com>
2022-07-13 14:06:48 -0500

    update

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -3,11 +3,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        res.setContentType("text/html");
-        res.getWriter().println("<h1>Hello World!</h1>");
+        //res.setContentType("text/html");
+        //res.getWriter().println("<h1>Hello World!</h1>");
+
+        String name = req.getParameter("name");
+        if (name != null) {
+            res.setContentType("text/html");
+            try {
+                PrintWriter out = res.getWriter();
+                out.println("<h1>Hello " + name + "</h1>");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            res.setContentType("text/html");
+            try {
+                PrintWriter out = res.getWriter();
+                out.println("<h1>Hello World</h1>");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
