0559c04e669f665accacd6424d7015f6e97ac65e
matthew jio <matthew.p.jio1@gmail.com>
2022-07-21 10:10:43 -0500

    wow

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,22 +1,66 @@
 import java.sql.Connection;
 import java.util.List;
 
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MySQLAdsDao implements Ads{
+
+    private Connection connection = null;
+
+    public MySQLAdsDao(){
+        try{
+            Config config = new Config();
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUsername(),
+                    config.getPassword()
+            );
+        }catch(SQLException e){
+            throw new RuntimeException("Error connecting to database.", e);
+        }
+    }
     @Override
-    public List<Ad> all() {
-        return null;
+    public List<Ad> all(){
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM ads");
+            List<Ad> ads = new ArrayList<>();
+            while(rs.next()){
+                ads.add(new Ad (
+                        rs.getLong("id"),
+                        rs.getLong("user_id"),
+                        rs.getString("title"),
+                        rs.getString("description")
+                ));
+            }
+            return ads;
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Long insert(Ad ad) {
-        return null;
-    }
+        try{
+            String query = "INSERT INTO ads (user_id, title, description) VALUES ('"
+                    + ad.getUserId() + "', '"
+                    + ad.getTitle() + "', '"
+                    + ad.getDescription() + "')";
 
-    // Define your constructor so that it accepts an instance of your Config class so that it can obtain the database credentials
-    public MySQLAdsDao(Config config) {
-        // Create a new instance of the MySQLConnection class
-        Connection connection = new Connection(config);
-        // Create a new instance of the MySQLAdsDao class
-        MySQLAdsDao dao = new MySQLAdsDao(connection);
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = stmt.getGeneratedKeys();
+            if(rs.next()){
+                System.out.println("Inserted a new record! New id: " + rs.getLong(1));
+            }
+        }catch(SQLException e){
+            throw new RuntimeException("Error found:" + e);
+        }
+        return ad.getUserId();
     }
 }
